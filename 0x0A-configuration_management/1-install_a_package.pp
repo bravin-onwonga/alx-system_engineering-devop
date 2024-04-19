5e91f2089618309bebac8112afe71ed737dbd843
bravin-onwonga <onbravin@gmail.com>
2024-04-20 00:30:46 +0300

    fix:Flask not flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Installs a package flask using pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'Flask':
+  ensure      => '2.1.0',
+  provider    => 'pip3',
+  configfiles => 'replace',
 }
