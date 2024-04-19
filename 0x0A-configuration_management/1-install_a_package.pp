6037234d3a0be118b308cfa9fe5cb830e8601579
bravin-onwonga <onbravin@gmail.com>
2024-04-19 23:09:09 +0300

    try:Using --force-reinstall command

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Installs a package flask using pip3
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+  ensure          => '2.1.0',
+  provider        => 'pip3',
   install_options => ['--force-reinstall'],
 }
