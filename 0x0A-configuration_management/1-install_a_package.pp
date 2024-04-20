a44462ae40b20e848311d429929954e45df5f70e
bravin-onwonga <onbravin@gmail.com>
2024-04-20 17:22:47 +0300

    fix:Installing Werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 # Installs python3-pip package
 package { 'python3-pip':
   ensure => installed,
+  name   => 'python3-pip',
+}
+
+package { 'Werkzeug':
+  ensure => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
 
 package { 'Flask':
