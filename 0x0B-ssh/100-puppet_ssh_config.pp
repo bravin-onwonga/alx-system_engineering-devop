e796de6aa17711870922d508d60fc5f5a5fc70a6
bravin-onwonga <onbravin@gmail.com>
2024-04-22 03:59:23 +0300

    Connect with private key

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,18 @@
+# Connect to your server using the private key
+exec { 'connect_server" :
+  command => 'echo ssh -i ~/.ssh/school ubuntu@server01',
+  path    => '/usr/bin/echo'
+}
+
 # Changes to our configuration file
 file { '/etc/ssh/sshd_config':
-  ensure => present,
+  ensure  => present,
   content => "Host *\n\tPasswordAuthentication no\n"
 }
 
 # Restart SSH service after making changes
 service { 'ssh':
-  ensure => running,
-  enable => true,
+  ensure  => running,
+  enable  => true,
   require => File['/etc/ssh/sshd_config'],
 }
