5486a5ac3d2b78756ad2c39ce2b7f73458754572
bravin-onwonga <onbravin@gmail.com>
2024-05-21 01:19:52 +0300

    fix:Getting titles from todos

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,22 +13,23 @@ if __name__ == "__main__":
         id = sys.argv[1]
         url = "https://jsonplaceholder.typicode.com/users/"
 
-        url_posts = url + id + "/posts"
         url_user = url + id
         url_todos = url + id + "/todos"
 
-        user_posts = requests.get(url_posts).json()
-        user_user_posts = requests.get(url_user).json()
-        url_todos = requests.get(url_todos).json()
-        for k in range(len(url_todos)):
-            if (url_todos[k].get('completed')):
+        user_info = requests.get(url_user).json()
+        user_todos = requests.get(url_todos).json()
+
+        for k in range(len(user_todos)):
+            if (user_todos[k].get('completed')):
                 count += 1
 
         total_todos = k + 1
         completed_todos = count
-        employee_name = user_user_posts.get('name')
+        employee_name = user_info.get('name')
         print("Employee {} is done with tasks({}/{}):".format(
             employee_name, completed_todos,
             total_todos))
-        for i in range(len(user_posts)):
-            print("\t", user_posts[i].get('title'))
+        for i in range(len(user_todos)):
+            if (user_todos[i].get('completed')):
+                print("\t", user_todos[i].get('title'))
+
