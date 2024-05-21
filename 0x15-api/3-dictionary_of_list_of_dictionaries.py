619f16d4abba41764195830133671ddf02b24402
bravin-onwonga <onbravin@gmail.com>
2024-05-21 04:38:04 +0300

    Records of all users in a single json file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,16 +9,30 @@ import sys
 
 
 if __name__ == "__main__":
-    if (len(sys.argv) and int(sys.argv[1]) <= 10):
-        id = str(sys.argv[1])
-        my_dct = {}
+    my_dct = {}
+    filename = "todo_all_employees.json"
+    url = "https://jsonplaceholder.typicode.com/"
+
+    url_todos = url + "/todos"
+
+    all_users_todos = requests.get(url_todos).json()
+
+    for i in range(1, 11):
         my_lst = []
-        user_todos = {}
-        filename = id + ".json"
-        url = "https://jsonplaceholder.typicode.com/"
-
-        url_user = url + "users?id=" + id
-        url_todos = url + "/todos"
-
+        url_user = url + "users?id=" + str(i)
         user_info = requests.get(url_user).json()
-        all_users_todos = requests.get(url_todos).json()
+        user_name = user_info[0]['username']
+        for user_todos in all_users_todos:
+            if user_todos['userId'] == i:
+                temp_dict = {
+                    'username': user_name,
+                    'task': user_todos.get('title'),
+                    'completed': user_todos.get('completed')
+                }
+                all_users_todos.remove(user_todos)
+            my_lst.append(temp_dict)
+        my_dct[i] = my_lst
+
+    with open(filename, 'w') as f:
+        data = json.dumps(my_dct)
+        f.write(data)
