e1027371d2121f7a32d05d112c47f501906aaa11
bravin-onwonga <onbravin@gmail.com>
2024-06-04 20:12:56 +0300

    Top ten hot topics

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,3 +12,20 @@ def top_ten(subreddit):
     if subreddit is None:
         return
     url = "https://reddit.com/r/{}/hot.json".format(subreddit)
+
+    try:
+        check = requests.head(url, allow_redirects=False)
+        check.raise_for_status()
+        res = requests.get(url)
+        my_lst = res.json().get('data').get('children')
+        i = 0
+        for dict in my_lst:
+            if i == 10:
+                break
+            print(dict.get('data').get('title'))
+            i += 1
+        return
+    except Exception:
+        print("None")
+        return
+
