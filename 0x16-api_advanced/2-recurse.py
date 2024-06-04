1f3ad6d952ae8fc2a09ac2d9727c41cf001e535a
bravin-onwonga <onbravin@gmail.com>
2024-06-05 00:22:43 +0300

    Sending requsts recursively

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Recursively queries the Reddit API
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Recursively sends requests to find the top posts of a subreddit
+    """
+    url = "https://reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'alx_task'}
+
+    try:
+        check = requests.head(url, allow_redirects=False)
+        check.raise_for_status()
+        params = {'after': after}
+        res = requests.get(url, headers=headers, params=params)
+        data = res.json().get('data')
+
+        if not data or not data.get('children'):
+            return hot_list
+        my_lst = data.get('children')
+        counter = 0
+        hot_list = add_titles(my_lst, counter, hot_list)
+        if not data.get('after'):
+            return hot_list
+        return (recurse(subreddit, hot_list, data.get('after')))
+    except Exception as e:
+        print("Error: {}".format(e))
+        return None
+
+
+def add_titles(lst, counter=0, hot_list=[]):
+    """Recursively adds the titles to the list"""
+    if counter == len(lst):
+        return hot_list
+    hot_list.append(lst[counter].get('data').get('title'))
+    return (add_titles(lst, counter + 1, hot_list))
